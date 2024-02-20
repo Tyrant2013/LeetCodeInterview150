7715af8c898d3d0f47848628601a409b30516775
Zhuang Xiaowei <zhuangxiaowei_dev@163.com>
2024-02-20 15:04:14 +0800

    添加一点信息

--- ContentView.swift
+++ ContentView.swift
@@ -10,7 +10,28 @@ import SwiftUI
 struct ContentView: View {
     let solutions: [Question] = Question.allCases
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            HStack(spacing: 2) {
+                Text("完成度")
+                Spacer()
+                Text("\(solutions.count)")
+                    .foregroundStyle(.green)
+                Text("/150")
+                    .foregroundStyle(.black)
+            }
+            .font(.system(size: 18))
+            
+            let process = CGFloat(solutions.count) / 150
+            RoundedRectangle(cornerRadius: 4)
+                .frame(height: 8)
+                .foregroundStyle(.gray.opacity(0.3))
+                .overlay(
+                    GeometryReader { proxy in
+                        RoundedRectangle(cornerRadius: 4)
+                            .foregroundStyle(.green)
+                            .frame(width: proxy.size.width * process)
+                    }
+                )
             List {
                 ForEach(solutions) { solution in
                     Link(destination: solution.link) { Text(solution.title) }
